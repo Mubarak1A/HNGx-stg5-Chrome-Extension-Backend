588e0661e43ec2bd3b2fefea0766e13c1df3b4d0
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-09-28 15:48:54 +0100

    write db connection config and add html for home route

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return '''
+    <html>
+        <body>
+            <h1>Hello, this is the chrome extension video backend!</h1>
+            <form action="/upload" method="post" enctype="multipart/form-data">
+                <input type="file" name="video">
+                <input type="submit" value="Upload">
+            </form>
+        </body>
+    </html>
+    '''
 
 
 app.run(host='0.0.0.0', port=81)
