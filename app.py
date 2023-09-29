67e9844a2af002b1f132343b64f5958c2f1bdf4a
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-09-29 15:13:30 +0100

    debug typo in /uploads/filename route

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def show_uploaded_videos():
     else:
         return "No videos uploaded yet."
 
-@app.route('/uploads/<filename>')
+@app.route('/uploads/<path:filename>')
 def uploaded_video(filename):
     filename = quote(filename)
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, mimetype='video/mp4')
