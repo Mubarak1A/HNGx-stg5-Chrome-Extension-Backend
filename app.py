26cc7ed0599836633909877c71bf07da75894bd7
Mubarak1A <adesinamubarak123@example.com>
2023-09-30 18:23:34 +0100

    rewrite route logics to work for streaming update

--- app.py
+++ app.py
@@ -1,50 +1,58 @@
-import os
-from flask import Flask, request, redirect, url_for, render_template, send_from_directory
-from urllib.parse import quote
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import requests
+import time
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///videos.db'  # SQLite database file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Video model to store video data
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.LargeBinary)
 
-uploaded_videos = []
+# Create the database and the Video table
+with app.app_context():
+    db.create_all()
 
-@app.route('/')
-def index():
-    return '''
-    <html>
-        <body>
-            <h1>Hello, this is the chrome extension video backend!</h1>
-            <form action="/upload" method="post" enctype="multipart/form-data">
-                <input type="file" name="video">
-                <input type="submit" value="Upload">
-            </form>
-        </body>
-    </html>
-    '''
+# Generate a new video ID and store an empty video record in the database
+def generate_video_id():
+    video = Video(data=b'')
+    db.session.add(video)
+    db.session.commit()
+    return video.id
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['video']
-    if file:
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        uploaded_videos.append(file.filename)
-        return redirect(url_for('index'))
+# Endpoint to start a new video and get a video ID
+@app.route('/start_video', methods=['POST'])
+def start_video():
+    video_id = generate_video_id()
+    return jsonify({'video_id': video_id})
 
-@app.route('/videos')
-def show_uploaded_videos():
-    if uploaded_videos:
-        return render_template('videos_render.html', video_filenames=uploaded_videos)
+# Endpoint to continuously update video data (simulate streaming)
+@app.route('/update_video/<int:video_id>', methods=['POST'])
+def update_video(video_id):
+    video = Video.query.get(video_id)
+
+    if video:
+        data = request.get_data()
+        video.data += data
+        db.session.commit()
+        return jsonify({'message': 'Update received.'})
     else:
-        return "No videos uploaded yet."
+        return jsonify({'error': 'Video not found.'}), 404
 
-@app.route('/uploads', methods=['GET'])
-def uploaded_video():
-    filename = str(request.args.get('filename'))
-    
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, mimetype='video/mp4')
+# Endpoint to get video data
+@app.route('/get_video/<int:video_id>', methods=['GET'])
+def get_video(video_id):
+    video = Video.query.get(video_id)
+
+    if video:
+        return jsonify({'video_data': video.data.decode('utf-8')})
+    else:
+        return jsonify({'error': 'Video not found.'}), 404
+
 
 if __name__ == '__main__':
-    # Create the upload directory if it doesn't exist
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     app.run(debug=True)
