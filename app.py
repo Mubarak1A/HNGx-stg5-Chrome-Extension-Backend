686bde9e89f1c4fa8f1d9be35e5b8c20dd60eccf
Adesina Mubarak <adesinamubarak123@gmail.com>
2023-10-01 01:19:33 +0100

    comment out cors

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+#CORS(app) 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///videos.db'  # SQLite database file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
